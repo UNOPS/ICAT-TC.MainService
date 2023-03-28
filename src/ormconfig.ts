e454ca724f1c38db425979cfe66accf55e582f93
Dilanka Ekanayake <dilanka@climatesi.com>
2023-03-28 11:24:32 +0530

    orm

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -8,17 +8,17 @@ export const config: ConnectionOptions = {
   port: 3306,
 
    username: 'sqluser',
-    password: 'password',
-    database: 'tc-main-new3',
+    // password: 'password',
+    // database: 'tc-main-new3',
 
  /* username: 'root',
  password: 'pradeep123#',
  database: 'tc-main', */
-  // password: 'dil19971121',
-  // database: 'climate_action',
+  password: '1997',
+  database: 'tc-main-new',
 
 
-  autoLoadEntities: true,
+ // autoLoadEntities: true,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
 
   // We are using migrations, synchronize should be set to false.
@@ -34,10 +34,10 @@ export const config: ConnectionOptions = {
   // __dirname is either dist or src folder, meaning either
   // the compiled js in prod or the ts in dev.
   migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
-  cli: {
-    // Location of migration should be inside src folder
-    // to be compiled into dist/ folder.
-    migrationsDir: 'src/migrations',
-  },
+  // cli: {
+  //   // Location of migration should be inside src folder
+  //   // to be compiled into dist/ folder.
+  //   migrationsDir: 'src/migrations',
+  // },
 };
 
