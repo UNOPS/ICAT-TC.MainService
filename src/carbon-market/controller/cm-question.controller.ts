527838c5dbee771fb96d0828dc2e8fbf9fe0ac7c
sandu9618 <sandunipavithra9611@gmail.com>
2023-04-25 10:35:52 +0530

    get section, criteria

--- src/carbon-market/controller/cm-question.controller.ts
+++ src/carbon-market/controller/cm-question.controller.ts
@@ -1,6 +1,6 @@
 import { Crud, CrudController } from "@nestjsx/crud";
 import { AssessmentCMDetail } from "../entity/assessment-cm-detail.entity";
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Query } from "@nestjs/common";
 import { AssessmentCMDetailService } from "../service/assessment-cm-detail.service";
 import { CMQuestion } from "../entity/cm-question.entity";
 import { CMQuestionService } from "../service/cm-question.service";
@@ -8,12 +8,18 @@ import { CMQuestionService } from "../service/cm-question.service";
 
 @Crud({
   model: {
-    type: AssessmentCMDetail,
+    type: CMQuestion,
   },
   query: {
     join: {
-      assessment: {
+      pre_question: {
         eager: true,
+      },
+      prev_answer_to_generate: {
+        eager: true
+      },
+      criteria: {
+        eager: true
       }
     },
   },
@@ -26,6 +32,17 @@ export class CMQuestionController implements CrudController<CMQuestion>
 
   get base(): CrudController<CMQuestion> {
     return this;
-}
+  }
+
+  @Get('get-all-section')
+  async getAllSection(){
+    return await this.service.getAllSection()
+  }
+
+  @Get('get-criteria-by-section')
+  async getCriteriaBySectionId(@Query('sectionId') sectionId: number){
+    return await this.service.getCriteriaBySectionId(sectionId)
+  }
+
 
 }
--- src/carbon-market/service/cm-question.service.ts
+++ src/carbon-market/service/cm-question.service.ts
@@ -1,16 +1,29 @@
 import { Injectable } from "@nestjs/common";
 import { TypeOrmCrudService } from "@nestjsx/crud-typeorm";
-import { AssessmentCMDetail } from "../entity/assessment-cm-detail.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import { CMQuestion } from "../entity/cm-question.entity";
-
+import { Criteria } from "../entity/criteria.entity";
+import { Repository } from "typeorm-next";
+import { Section } from "../entity/section.entity";
 
 @Injectable()
 export class CMQuestionService extends TypeOrmCrudService<CMQuestion> {
   constructor(
-    @InjectRepository(CMQuestion) repo
+    @InjectRepository(CMQuestion) repo,
+    @InjectRepository(Criteria)
+    private criteriaRepo: Repository<Criteria>,
+    @InjectRepository(Section)
+    private sectionRepo: Repository<Section>
   ) {
     super(repo);
+  }
+
+  async getAllSection():Promise<Section[]>{
+    return await this.sectionRepo.find()
+  }
+
+  async getCriteriaBySectionId(sectionId: number):Promise<Criteria[]>{
+    return await this.criteriaRepo.find({section: {id: sectionId}})
   }
 }
 
