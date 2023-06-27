4d0696f6433495809f5fde1dc9e20867ec10844a
Dilanka Ekanayake <dilanka@climatesi.com>
2023-06-28 00:43:23 +0530

    CM-result page

--- src/carbon-market/service/cm-question.service.ts
+++ src/carbon-market/service/cm-question.service.ts
@@ -7,7 +7,7 @@ import { Section } from "../entity/section.entity";
 import { CMAnswer } from "../entity/cm-answer.entity";
 import { Repository } from "typeorm";
 import { categories, characteristic, questions } from "../dto/seed-data";
-import { UniqueCharacteristicDto } from "../dto/unique-characteristic.dto";
+// import { UniqueCharacteristicDto } from "../dto/unique-characteristic.dto";
 
 @Injectable()
 export class CMQuestionService extends TypeOrmCrudService<CMQuestion> {
