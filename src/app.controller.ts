175d545d2c441c24597993bd8e857e03f6cf88e7
Pradeep_Ranasinghe <pradeep@climatesi.com>
2023-07-28 11:15:23 +0530

    pipeline

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { exec } from 'child_process';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,21 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('deploy')
+  deploy(){
+    console.log("deloying the main service ---------------------------------")
+    exec('sh /home/ubuntu/code/main-deploy.sh',
+        (error, stdout, stderr) => {
+            console.log(stdout);
+            console.log(stderr);
+            if (error !== null) {
+                console.log(`exec error: ${error}`);
+            }
+        });       
+
+    return true;
+
   }
 }
