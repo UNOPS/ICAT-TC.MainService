52271e42bdad25dd7363ab4b36e69148ae64ab4e
Dilanka Ekanayake <dilanka@climatesi.com>
2023-09-22 19:51:36 +0530

    syn true

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -85,7 +85,7 @@ ParameterHistory,Report,BaseTrackingEntity,MasterData,User,MethodologyParameters
 PolicySector,InvestorQuestions,IndicatorDetails,PortfolioSdg,SdgAssessment,BarrierCategory,PortfolioQuestions,PortfolioQuestionDetails],
 
   // We are using migrations, synchronize should be set to false.
-  synchronize: false,
+  synchronize: true,
 
   // Run migrations automatically,
   // you can disable this if you prefer running migration manually.
