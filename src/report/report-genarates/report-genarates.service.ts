70239fb43134e2d174958dfe87b080598b081cc7
Dilanka Ekanayake <dilanka@climatesi.com>
2023-11-29 13:25:23 +0530

    path

--- src/report/report-genarates/report-genarates.service.ts
+++ src/report/report-genarates/report-genarates.service.ts
@@ -12,8 +12,8 @@ constructor(){}
     let options = {
       format: 'A4',
       margin: { top: '0px', bottom: '0px', left: '0px', right: '0px' },
-      //  path: '/home/ubuntu/code/Main/main/public/' + fileName,
-      path:  './public/' + fileName,
+       path: '/home/ubuntu/code/Main/main/public/' + fileName,
+      // path:  './public/' + fileName,
       printBackground: true
     };
 
