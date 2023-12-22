be7009d2f41bdc14085b242a7f715bdcbd8d8b98
Kashul <kashul@climatesi.com>
2023-12-22 11:06:52 +0530

    update

--- src/report/report-html-genarates/report-html-genarates.service.ts
+++ src/report/report-html-genarates/report-html-genarates.service.ts
@@ -50,7 +50,7 @@ export class ReportHtmlGenaratesService {
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="http://localhost:7080/report/css/reportserver.css">
+        <link rel="stylesheet" href="http://localhost:7080/report/css/reportserverlandscape.css">
             </head>
   
   
@@ -102,7 +102,7 @@ export class ReportHtmlGenaratesService {
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="http://localhost:7080/report/css/reportserver.css">
+        <link rel="stylesheet" href="http://localhost:7080/report/css/reportserverlandscape.css">
             </head>
   
   
@@ -168,7 +168,7 @@ let comparisonContentFour=this.reportPagesService.comparisonContentFour(header,f
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="http://localhost:7080/report/css/reportserver.css">
+        <link rel="stylesheet" href="http://localhost:7080/report/css/reportserverlandscape.css">
             </head>
   
   
