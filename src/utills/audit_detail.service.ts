8774ab0981e1d86d96f87eaf82912035f8da112d
sanduni pavithra <sp96.info@gmail.com>
2024-01-05 17:28:14 +0530

    userType enum

--- src/utills/audit_detail.service.ts
+++ src/utills/audit_detail.service.ts
@@ -6,6 +6,12 @@ import { HttpService } from "@nestjs/axios";
 @Injectable()
 export class AuditDetailService {
     public auditlogURL = process.env.AUDIT_URL + '/audit/log'
+    public UserTypes =  [
+        {name: "Country Admin", code: UserTypesEnum.COUNTRY_ADMIN},
+        {name: "Country User", code: UserTypesEnum.COUNTRY_USER},
+        {name: "Master Admin", code: UserTypesEnum.MASTER_ADMIN},
+        {name: "External", code: UserTypesEnum.EXTERNAL}
+    ]
 
     constructor(
         private readonly usersService: UsersService,
@@ -16,10 +22,18 @@ export class AuditDetailService {
     async getAuditDetails() {
         let [userName] = this.tokenDetails.getDetails([TokenReqestType.username])
         let user = await this.usersService.findByUserName(userName)
+        let userType: string
+
+        let type = this.UserTypes.find(o => o.name === user.userType.name)
+        if (type) {
+            userType = type.code
+        } else {
+            userType = user.userType.name
+        }
 
         return {
             userName: user.username,
-            userType: user.userType.name,
+            userType: userType,
             uuId: user.id,
             institutionId: user.institution.id,
             countryId: user.country.id
@@ -33,4 +47,11 @@ export class AuditDetailService {
             console.log("log failed ")
         }
     }
+}
+
+export enum UserTypesEnum {
+    COUNTRY_ADMIN = "COUNTRY_ADMIN",
+    COUNTRY_USER = "COUNTRY_USER",
+    MASTER_ADMIN = "MASTER_ADMIN",
+    EXTERNAL = "EXTERNAL"
 }
