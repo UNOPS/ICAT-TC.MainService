cda50f04f69a8ed617694cdd2f6545d36eba4636
Kashul <kashul@climatesi.com>
2024-01-17 17:23:45 +0530

    update

--- src/document/document.module.ts
+++ src/document/document.module.ts
@@ -3,11 +3,12 @@ import { Module } from '@nestjs/common';
 import { DocumentService } from './document.service';
 import { DocumentController } from './document.controller';
 import { Documents } from './entity/document.entity';
+import { StorageService } from 'src/storage/storage.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Documents])],
-  providers: [DocumentService],
+  providers: [DocumentService,StorageService],
   controllers: [DocumentController],
-  exports: [DocumentService],
+  exports: [DocumentService,StorageService],
 })
 export class DocumentModule {}
