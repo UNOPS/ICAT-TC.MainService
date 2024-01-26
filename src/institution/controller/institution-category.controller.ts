3f49da3404067e94f938b304999de0aa1f82044f
Pradeep_Ranasinghe <pradeep@climatesi.com>
2024-01-26 17:35:45 +0530

    iss

--- src/institution/controller/institution-category.controller.ts
+++ src/institution/controller/institution-category.controller.ts
@@ -1,7 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards, Request } from '@nestjs/common';
 import { Crud, CrudController } from '@nestjsx/crud';
 import { InstitutionCategoryService } from '../service/institution-category.service';
 import { InstitutionCategory } from '../entity/institution.category.entity';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 
 @Crud({
@@ -12,4 +13,13 @@ import { InstitutionCategory } from '../entity/institution.category.entity';
 @Controller('institution-category')
 export class InstitutionCategoryController implements CrudController<InstitutionCategory>{
         constructor(public service: InstitutionCategoryService){}
+
+
+        @UseGuards(JwtAuthGuard)
+        @Get('getInstitutionCategory')
+        async getInstitutionType(
+        ): Promise<any> {
+            return await this.service.getInstitution(
+            );
+        }
     }
