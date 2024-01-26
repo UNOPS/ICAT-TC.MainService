d70706d9162ada9e3f03244c1b73ce5954e67ae1
Dilanka Ekanayake <dilanka@climatesi.com>
2024-01-26 16:38:08 +0530

    intervention issues

--- src/master-data/project-status/project-status.service.ts
+++ src/master-data/project-status/project-status.service.ts
@@ -5,7 +5,13 @@ import { ProjectStatus } from './project-status.entity';
 
 @Injectable()
 export class ProjectStatusService extends TypeOrmCrudService<ProjectStatus> {
+ 
   constructor(@InjectRepository(ProjectStatus) repo) {
     super(repo);
+  }
+
+  async getAllProjectStatus(): Promise<ProjectStatus[]> {
+      let data = this.repo.createQueryBuilder('projectStatus').getMany()
+      return await data
   }
 }
