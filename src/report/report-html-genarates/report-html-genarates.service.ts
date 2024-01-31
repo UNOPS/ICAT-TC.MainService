8fed74acac4cb6520fcdaeed3879ad66206ec7f8
Kashul <kashul@climatesi.com>
2024-01-31 16:51:55 +0530

    update

--- src/report/report-html-genarates/report-html-genarates.service.ts
+++ src/report/report-html-genarates/report-html-genarates.service.ts
@@ -165,7 +165,7 @@ let comparisonContentFour=this.reportPagesService.comparisonContentFour('','',re
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="http://localhost:7080/report/css/reportserverlandscapewithoutpage.css">
+        <link rel="stylesheet" href="${process.env.MAIN_URL}/report/css/reportserverlandscapewithoutpage.css">
             </head>
   
   
