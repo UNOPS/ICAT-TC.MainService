8a95bcad183dc558bbd79342611eb6cee2aebb18
Kashul <kashul@climatesi.com>
2024-06-10 19:30:59 +0530

    update

--- src/report/report-genarates/report-genarates.service.ts
+++ src/report/report-genarates/report-genarates.service.ts
@@ -15,9 +15,9 @@ constructor(){}
    
       printBackground: true,
       landscape:true,
+      displayHeaderFooter: true,
      headerTemplate: "<div/>",
-     footerTemplate: "<div style=\"text-align: right;width: 297mm;font-size: 8px;\"><span style=\"margin-right: 1cm\"><span class=\"pageNumber\"></span> of <span class=\"totalPages\"></span></span></div>"
-    
+     footerTemplate: "<div style=\"text-align: right;width: 297mm;font-size: 8px;\"><span style=\"margin-right: 1cm\"><span class=\"pageNumber\"></span> of <span class=\"totalPages\"></span></span></div>"    
     };
 
     
